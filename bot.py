fba6851d2eadfbf3fa6b5c36f71e85aebd767ec2
Blanca <blancacano2.71828@gmail.com>
2018-07-18 20:24:53 +0200

    questions

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, CommandHandler
 from config import TOKEN
 import random as rd
 
-TOTAL_QUESTIONS = 475
+TOTAL_QUESTIONS = 474
 QUESTIONS = 'questions.txt'
 
 def ask(bot, update):
